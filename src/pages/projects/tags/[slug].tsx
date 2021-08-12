edc53dbc2e97e4d69bb1f84780b45d2e9d321908
Adam Rafiandri <rafiandria23@gmail.com>
2021-08-12 14:52:59 +0700

    WEB-2
    - Created Tag page for Projects.
    - Added Projects and Articles attributes in Tag interface.

--- src/pages/projects/tags/[slug].tsx
+++ src/pages/projects/tags/[slug].tsx
@@ -1,11 +1,16 @@
-import { NextPage, GetStaticProps } from 'next';
+import {
+  NextPage,
+  GetStaticPaths,
+  GetStaticPathsResult,
+  GetStaticProps,
+} from 'next';
 import { NextSeo } from 'next-seo';
 import { gql } from '@apollo/client';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Grid, Typography, Divider } from '@material-ui/core';
 
 // Types
-import { Project } from '@/types/project';
+import { Tag } from '@/types/tag';
 
 // GraphQL
 import { client } from '@/graphql';
@@ -14,18 +19,18 @@ import { client } from '@/graphql';
 import { Layout } from '@/components';
 import { ProjectCard } from '@/components/project';
 
-interface ProjectsPageProps {
-  projects: Project[];
+interface ProjectTagsPageProps {
+  tag: Tag;
 }
 
-const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects }) => {
+const ProjectTagsPage: NextPage<ProjectTagsPageProps> = ({ tag }) => {
   const classes = useStyles();
 
   return (
     <>
       <NextSeo
-        title={`My Projects`}
-        description={`All the projects I'm currently doing or already done, ranging from Back-End, Front-End, to Full-Stack`}
+        title={tag.name}
+        description={`Project that are made with ${tag.name}`}
       />
 
       <Layout>
@@ -43,7 +48,7 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects }) => {
               variant={`h5`}
               gutterBottom
             >
-              {`My Projects`}
+              {`${tag.name} Projects`}
             </Typography>
           </Grid>
 
@@ -60,8 +65,8 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects }) => {
             justifyContent={`space-evenly`}
             alignItems={`stretch`}
           >
-            {projects !== undefined && projects.length > 0
-              ? projects.map((project) => {
+            {tag.projects !== undefined && tag.projects.length > 0
+              ? tag.projects.map((project) => {
                   return (
                     <Grid item key={project._id}>
                       <ProjectCard project={project} />
@@ -76,39 +81,67 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps<ProjectsPageProps> = async () => {
-  const { data } = await client.query<{ projects: Project[] }>({
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query<{ tags: Tag[] }>({
     query: gql`
       query {
-        projects {
-          _id
-          title
+        tags {
           slug
-          overview
-          cover {
-            url
-            width
-            height
-          }
-          tags {
-            _id
-            name
-            slug
-          }
         }
       }
     `,
   });
 
+  const slugs: GetStaticPathsResult['paths'] = data.tags.map((tag) => ({
+    params: {
+      slug: tag.slug,
+    },
+  }));
+
+  return {
+    paths: slugs,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<
+  ProjectTagsPageProps,
+  { slug: Tag['slug'] }
+> = async ({ params }) => {
+  const slug = params?.slug;
+  const { data } = await client.query<{ tags: Tag[] }>({
+    query: gql`
+      query ($slug: String!) {
+        tags(where: { slug: $slug }) {
+          name
+          projects {
+            _id
+            title
+            slug
+            overview
+            cover {
+              url
+              width
+              height
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      slug,
+    },
+  });
+
   return {
     props: {
-      projects: data.projects,
+      tag: data.tags[0],
     },
     revalidate: 1,
   };
 };
 
-export default ProjectsPage;
+export default ProjectTagsPage;
 
 const useStyles = makeStyles((theme) =>
   createStyles({
--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,5 +1,10 @@
+import { Project } from './project';
+import { Article } from './article';
+
 export interface Tag {
   _id: string;
   name: string;
   slug: string;
+  projects: Project[];
+  articles: Article[];
 }
